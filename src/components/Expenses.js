28a584e9592fdb6111d1b148fa70d1e9f416a5b4
Wilfred <wilfred05777@gmail.com>
2021-04-22 18:23:43 +0800

    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/practice/1289990/instructor-solution#overview

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,30 +1,27 @@
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: "94.12",
-      date: new Date(2021, 2, 14),
-    },
-    {
-      id: "e2",
-      title: "New Super Computer",
-      amount: "294.12",
-      date: new Date(2001, 3, 14),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: "323.12",
-      date: new Date(2002, 7, 14),
-    },
-  ];
-
   return (
-    <div>
+    <div className="expenses">
       <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      ></ExpenseItem>
+    </div>
+
+    /* <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
@@ -38,8 +35,7 @@ function Expenses(props) {
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
-    </div>
+      ></ExpenseItem> */
   );
 }
 
